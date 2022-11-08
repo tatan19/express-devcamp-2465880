30e2842c94d2a7fdf1dc95559734ca586c8d07ce
HarrisonIbarra <hibarra65@misena.edu.co>
2022-11-08 09:51:36 -0500

    Validaciones

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,10 +3,11 @@
 const sequelize = require('../config/seq')
 
 //datatypes
-const { DataTypes } = require('sequelize')
+const { DataTypes, ValidationError } = require('sequelize')
 
 //el modelo
 const UserModel = require('../models/user')
+const user = require('../models/user')
 
 //crear el objeto usuario
 const User = UserModel(sequelize, DataTypes)
@@ -17,56 +18,125 @@ const User = UserModel(sequelize, DataTypes)
 
 //get:obtener detos Red
 exports.traerUsers = async (req, res) => {
-    const users = await User.findAll();
-    res.status(200).json(
-        {
-            "succes": true,
-            "data": users
-        }
-    )
+    try {
+        const users = await User.findAll();
+        res.status(200).json(
+            {
+                "succes": true,
+                "data": users
+            }
+        )
+    } catch (error) {
+        res.status(500)
+            .json({
+                "succes": false,
+                "errors": "error de servidor"
+            })
+    }
 }
 
 //obtener recursos por get
 exports.traerUserPorId = async (req, res) => {
-    const userId = await User.findByPk(req.params.id)
-    res.status(200).json(
-        {
-            "succes": true,
-            "data": userId
+
+    try {
+        const userId = await User.findByPk(req.params.id)
+
+        if (!userId) {
+            res.status(422).json(
+                {
+                    "succes": false,
+                    "errors": [
+                        "Usuario no existe"
+                    ]
+                }
+            )
+        } else {
+            res.status(200).json(
+                {
+                    "succes": true,
+                    "data": userId
+                }
+            )
         }
-    )
+    } catch (error) {
+        res.status(500)
+            .json({
+                "succes": false,
+                "errors": "Error de servidor"
+            })
+    }
+
 
 }
 
 //POST: Crear un nuevo recurso
 exports.crearUser = async (req, res) => {
-    const newUser = await User.create(req.body);
-    res.status(201).json(
-        {
-            "succes": true,
-            "data": newUser
+    try {
+        const newUser = await User.create(req.body);
+        res.status(201).json(
+            {
+                "succes": true,
+                "data": newUser
+            }
+        )
+    } catch (error) {
+
+        if (error instanceof ValidationError) {
+            //Mensajes de eerores
+            const errores = error.errors.map((e) => e.message)
+            res.status(422)
+                .json({
+                    "succes": false,
+                    "errors": errores
+                })
+        } else {
+            res.status(500)
+                .json({
+                    "succes": false,
+                    "errors": "error de servidor"
+                })
         }
-    )
+    }
+
 }
 
 //PUT
 exports.actualizarUser = async (req, res) => {
-    //actializar usuario por id
-    await User.update(req.body, {
-        where: {
-            id: req.params.id
+    try {
+        const upUser = await User.findByPk(req.params.id)
+        if (!upUser) {
+            //response de usuario no encontrado
+            res.status(422).json(
+                {
+                    "succes": false,
+                    "errors": [
+                        "Usuario no existe"
+                    ]
+                }
+            )
+        } else {
+            //actializar usuario por id
+            await User.update(req.body, {
+                where: {
+                    id: req.params.id
+                }
+            });
+            const userAct = await User.findByPk(req.params.id)
+            res.status(200).json(
+                {
+                    "succes": true,
+                    "data": userAct
+                }
+            )
         }
-    });
+    } catch (error) {
+        res.status(500)
+            .json({
+                "succes": false,
+                "errors": "error de servidor"
+            })
+    }
 
-    //consultar datos actualizados
-    const upUser = await User.findByPk(req.params.id)
-
-    res.status(200).json(
-        {
-            "succes": true,
-            "data": upUser
-        }
-    )
 }
 
 
--- models/user.js
+++ models/user.js
@@ -14,9 +14,58 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   User.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isAlpha: {
+          args: true,
+          msg: 'Name solo debe tener letras'
+        },
+        notEmpty: {
+          args: true,
+          msg: 'Name no debe quedar vacio'
+        },
+        notNull: {
+          args: true,
+          msg: 'No debe ser nulo'
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: 'Name no debe quedar vacio'
+        },
+        notNull: true,
+        isEmail: {
+          args: true,
+          msg: "email debe ser valido"
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [2, 10],
+          msg: "Password entre 5 y 10 caracteres"
+        },
+        notEmpty: {
+          args: true,
+          msg: 'password no debe estar vacia'
+        },
+        notNull: {
+          args: true,
+          msg: 'Password no debe ser nula'
+        }
+      }
+    }
   }, {
     sequelize,
     modelName: 'User',
